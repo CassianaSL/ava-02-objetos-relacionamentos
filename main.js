525832dd679c797f0bf765bc2d668aa42fd2771e
Marcio Torres <marcio.torres@riogrande.ifrs.edu.br>
2018-08-27 20:16:14 -0300

    count retificado

--- main.js
+++ main.js
@@ -21,8 +21,8 @@ console.assert(Gandalf.toString() === 'Gandalf');
 // There is no members yet
 console.assert(FellowshipOfTheRing.toString() === 'Fellowship of The Ring');
 console.assert(EvilFellowship.toString() === 'Fellowship of Evil');
-console.assert(EvilFellowship.count === 0);
-console.assert(FellowshipOfTheRing.count === 0);
+console.assert(EvilFellowship.count() === 0);
+console.assert(FellowshipOfTheRing.count() === 0);
 console.assert(FellowshipOfTheRing.hasNoMembers === true);
 console.assert(FellowshipOfTheRing.hasMembers === false);
 
@@ -33,7 +33,7 @@ console.assert(FellowshipOfTheRing.lastMember === undefined);
 // There is two members (order matters)
 FellowshipOfTheRing.signUp(Gandalf); // member 1
 FellowshipOfTheRing.signUp(Aragorn); // member 2
-console.assert(FellowshipOfTheRing.count === 2);
+console.assert(FellowshipOfTheRing.count() === 2);
 console.assert(FellowshipOfTheRing.hasNoMembers === false);
 console.assert(FellowshipOfTheRing.hasMembers === true);
 
@@ -66,7 +66,7 @@ console.assert(Gimli.isMemberOfTheFellowship(EvilFellowship) === false);
 
 // However, a person can be member of only one fellowship
 EvilFellowship.signUp(Aragorn); // Aragorn has not been included
-console.assert(EvilFellowship.count === 0);
+console.assert(EvilFellowship.count() === 0);
 console.assert(EvilFellowship.hasNoMembers === true);
 console.assert(Aragorn.fellowship === FellowshipOfTheRing);
 console.assert(Aragorn.isMemberOfTheFellowship(FellowshipOfTheRing) === true);
@@ -75,12 +75,12 @@ console.assert(Aragorn.isMemberOfTheFellowship(EvilFellowship) === false);
 // There is another way to join the fellowships
 Gimli.join(FellowshipOfTheRing);
 Gimli.join(EvilFellowship); // has no effect (already in a fellowship)
-console.assert(FellowshipOfTheRing.count === 3);
+console.assert(FellowshipOfTheRing.count() === 3);
 console.assert(FellowshipOfTheRing.member(3).person === Gimli);
 
 // Even indirect way
 Legolas.join(Gimli.fellowship);
-console.assert(FellowshipOfTheRing.count === 4);
+console.assert(FellowshipOfTheRing.count() === 4);
 console.assert(FellowshipOfTheRing.lastMember.person === Legolas);
 
 // More queries
@@ -91,39 +91,39 @@ console.assert(FellowshipOfTheRing.has('Human') === true);
 console.assert(FellowshipOfTheRing.has('Hobbit') === false);
 
 // Get the fellowship complete (adding various members at one time)
-console.assert(FellowshipOfTheRing.count === 4);
+console.assert(FellowshipOfTheRing.count() === 4);
 FellowshipOfTheRing.signUp(Boromir, Frodo);
 FellowshipOfTheRing.signUp(Sam, Meriadoc, Peregrin);
-console.assert(FellowshipOfTheRing.count === 9);
+console.assert(FellowshipOfTheRing.count() === 9);
 console.assert(FellowshipOfTheRing.count('Hobbit') === 4);
 
 // Left the FellowshipOfTheRing
-console.assert(FellowshipOfTheRing.count === 9);
+console.assert(FellowshipOfTheRing.count() === 9);
 console.assert(FellowshipOfTheRing.count('Human') === 2);
 console.assert(Boromir.fellowship === FellowshipOfTheRing);
 console.assert(FellowshipOfTheRing.member(5).person === Boromir);
 
 FellowshipOfTheRing.cancel(Boromir);
 
-console.assert(FellowshipOfTheRing.count === 8);
+console.assert(FellowshipOfTheRing.count() === 8);
 console.assert(FellowshipOfTheRing.count('Human') === 1);
 console.assert(Boromir.fellowship === undefined);
 console.assert(FellowshipOfTheRing.member(5).person === Frodo);
 
 // Other way to unsubscribe
 Frodo.left(); // leave current fellowship if any
-console.assert(FellowshipOfTheRing.count === 7);
+console.assert(FellowshipOfTheRing.count() === 7);
 console.assert(Frodo.fellowship === undefined);
 
 Gandalf.die();
-console.assert(FellowshipOfTheRing.count === 6);
+console.assert(FellowshipOfTheRing.count() === 6);
 console.assert(Gandalf.fellowship === undefined);
 
 // People can't join fellowships after die
-console.assert(FellowshipOfTheRing.count === 6);
+console.assert(FellowshipOfTheRing.count() === 6);
 Gandalf.join(FellowshipOfTheRing);
 console.assert(Gandalf.fellowship === undefined);
-console.assert(FellowshipOfTheRing.count === 6);
+console.assert(FellowshipOfTheRing.count() === 6);
 
 // Asking if people are sharing a fellowship
 console.assert(Meriadoc.isFellowOf(Peregrin) === true);
@@ -148,7 +148,7 @@ console.assert(Boromir.fellowship === Meriadoc.fellowship);
 
 // Dissolve the fellowship :(
 FellowshipOfTheRing.dissolve();
-console.assert(FellowshipOfTheRing.count === 0);
+console.assert(FellowshipOfTheRing.count() === 0);
 console.assert(FellowshipOfTheRing.hasNoMembers === true);
 console.assert(FellowshipOfTheRing.member(1) === undefined);
 console.assert(FellowshipOfTheRing.lastMember === undefined);
